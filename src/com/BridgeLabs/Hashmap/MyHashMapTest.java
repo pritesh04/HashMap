0c29de1e85481b4ec53e3f05bd8bbe92ffd53340
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-27 00:52:12 +0530

    [pritesh] Found frequency of each Words in a paragraph

--- src/com/BridgeLabs/Hashmap/MyHashMapTest.java
+++ src/com/BridgeLabs/Hashmap/MyHashMapTest.java
@@ -17,7 +17,27 @@ public class MyHashMapTest {
 
 		}
 		System.out.println(hashMap);
+		System.out.println("----------");
 
+		String b = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		b = b.toLowerCase();
+		MyHashMap<String, Integer> hashMap1 = new MyHashMap<>();
+		String[] word = b.split(" ");
+		for (String string : word) {
+			Integer val = hashMap1.get(string);
+			if (val == null)
+				val = 1;
+			else
+				val = val + 1;
+			
+			hashMap1.add(string, val);
+			
+			
+
+		}
+		
+		System.out.println(hashMap1);
+		
 
 //		MyHashMap<String, Integer> hashMap = new MyHashMap<>();
 //		hashMap.add("1", 12345);
